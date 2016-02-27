85e3d88d5e541ea7cf2e5b7d1b30fc04ed27d900
Roberto Torella <roberto.torella@gmail.com>
2016-02-27 12:04:52 +0000

    Improved code climate

--- src/Memoizer.php
+++ src/Memoizer.php
@@ -2,29 +2,33 @@
 
 namespace ganglio;
 
-class Memoizer {
+class Memoizer
+{
 
-	const NOT_CALLABLE = 1;
+  const NOT_CALLABLE = 1;
 
-	private $f = null;
-	private $results = null;
+  private $f = null;
+  private $results = null;
 
-	public function __construct($f) {
-		if (!is_callable($f)) {
-			throw new \InvalidArgumentException("Argument needs to be callable", self::NOT_CALLABLE);
-		}
+  public function __construct($f)
+  {
+    if (!is_callable($f)) {
+      throw new \InvalidArgumentException("Argument needs to be callable", self::NOT_CALLABLE);
+    }
 
-		$this->f = new \ReflectionFunction($f);
-	}
+    $this->f = new \ReflectionFunction($f);
+  }
 
-	public function __invoke() {
-		$args = func_get_args();
-		$args_hash = spl_object_hash((object)$args);
+  public function __invoke()
+  {
+    $args = func_get_args();
+    $args_hash = spl_object_hash((object)$args);
 
-		if (!isset($this->results[$args_hash])) {
-			$this->results[$args_hash] = $this->f->invokeArgs($args);
-		}
+    if (!isset($this->results[$args_hash])) {
+      $this->results[$args_hash] = $this->f->invokeArgs($args);
+    }
 
-		return $this->results[$args_hash];
-	}
+    return $this->results[$args_hash];
+  }
+
 }
--- test/src/MemoizerTest.php
+++ test/src/MemoizerTest.php
@@ -4,68 +4,75 @@ namespace ganglio\tests;
 
 use \ganglio\Memoizer;
 
-class MemoizerTest extends \PHPUnit_Framework_TestCase{
+class MemoizerTest extends \PHPUnit_Framework_TestCase
+{
 
-	protected $func;
+    protected $func;
 
-	protected function setUp() {
+    protected function setUp()
+    {
 
-		$this->func = function($a) {
-			return $a * mt_rand();
-		};
+        $this->func = function ($a) {
+            return $a * mt_rand();
+        };
 
-		$this->slow_func = function ($a) {
-			sleep(5);
-			return $a * mt_rand();
-		};
-	}
+        $this->slow_func = function ($a) {
+            sleep(5);
+            return $a * mt_rand();
+        };
+    }
 
-	protected function tearDown() {
-		$this->func = null;
-	}
+    protected function tearDown()
+    {
+        $this->func = null;
+    }
 
-	/**
-	 * @expectedException InvalidArgumentException
-	 * excectedExceptionCode 1
-	 */
-	public function testConstructionArgumentNotCallable() {
-		new Memoizer(33);
-	}
+    /**
+     * @expectedException InvalidArgumentException
+     * excectedExceptionCode 1
+     */
+    public function testConstructionArgumentNotCallable()
+    {
+        new Memoizer(33);
+    }
 
-	public function testReturnsCallable() {
-		$this->assertTrue(
-			is_callable(new Memoizer($this->func))
-		);
-	}
+    public function testReturnsCallable()
+    {
+        $this->assertTrue(
+            is_callable(new Memoizer($this->func))
+        );
+    }
 
-	public function testReturnSame() {
-		$me = new Memoizer($this->func);
+    public function testReturnSame()
+    {
+        $me = new Memoizer($this->func);
 
-		$this->assertEquals(
-			$me(1),
-			$me(1)
-		);
-	}
+        $this->assertEquals(
+            $me(1),
+            $me(1)
+        );
+    }
 
-	public function testUseCache() {
-		$me = new Memoizer($this->slow_func);
+    public function testUseCache()
+    {
+        $me = new Memoizer($this->slow_func);
 
-		$ts = time();
-		$me(1);
-		$dt_first_run = time() - $ts;
+        $ts = time();
+        $me(1);
+        $dt_first_run = time() - $ts;
 
-		$ts = time();
-		$me(1);
-		$dt_second_run = time()- $ts;
+        $ts = time();
+        $me(1);
+        $dt_second_run = time()- $ts;
 
-		$this->assertEquals(
-			$dt_first_run,
-			5
-		);
-		$this->assertEquals(
-			$dt_second_run,
-			0
-		);
-	}
+        $this->assertEquals(
+            $dt_first_run,
+            5
+        );
+        $this->assertEquals(
+            $dt_second_run,
+            0
+        );
+    }
 
 }
